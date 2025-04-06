b72660877e8adb8c0b504e4761a5c90c1e18cde3
Drogshell <89346155+Drogshell@users.noreply.github.com>
2025-04-06 17:07:57 +1000

    Main Layout and functionality
    - Home screen layout with tabs and floating action button are working
    - Displaying a fragment for the main task view

--- app/src/main/java/com/trevin/ticknow/MainActivity.kt
+++ app/src/main/java/com/trevin/ticknow/MainActivity.kt
@@ -1,20 +1,68 @@
 package com.trevin.ticknow
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.tabs.TabLayoutMediator
+import com.trevin.ticknow.databinding.ActivityMainBinding
+import com.trevin.ticknow.databinding.DialogAddTaskBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.pager.adapter = SwipePageAdapter(this)
+        TabLayoutMediator(binding.tabs, binding.pager){ tab, position ->
+            tab.text = "Tasks"
+        }.attach()
+
+        binding.fab.setOnClickListener {
+            showAddTaskDialog()
+        }
+    }
+
+    private fun showAddTaskDialog() {
+        val dialogBinding = DialogAddTaskBinding.inflate(layoutInflater)
+        MaterialAlertDialogBuilder(this)
+            .setTitle("Add New Task")
+            .setView(dialogBinding.root)
+            .setPositiveButton("Save") { _, _ ->
+                Toast.makeText(
+                    this,
+                    "Your Task is: ${dialogBinding.editText.text}",
+                    Toast.LENGTH_LONG
+                ).show()
+
+            }
+            .setNegativeButton("Cancel", null)
+            .show()
+    }
+
+    inner class SwipePageAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
+        override fun createFragment(position: Int): Fragment {
+            return TasksFragment()
+        }
+
+        override fun getItemCount(): Int {
+            return 1
         }
     }
 }
